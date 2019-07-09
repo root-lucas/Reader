efc23d1ca2fbeb7554d0ab1d157d7277fca64b49
root-lucas <972406415@qq.com>
2019-07-09 13:55:22 +0800

    titleAndMenuBar

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,24 +1,63 @@
 <template>
 	<div class="ebook">
+	<transition name="slide-down">
+		<div class="title-wrapper" v-show="ifTitleAndMenuShow">
+			<div class="left">
+				<span class="iconfont icon">&#xe617;</span>
+			</div>
+			<div class="right">
+				<div class="icon-wrapper icon">
+					<span class="iconfont">&#xe61d;</span>
+				</div>
+				<div class="icon-wrapper">
+					<span class="iconfont icon">&#xe63a;</span>
+				</div>
+				<div class="icon-wrapper">
+					<span class="iconfont icon">&#xe65b;</span>
+				</div>
+			</div>
+		</div>
+	</transition>
 		<div class="read-wrapper">
 			<div id='read'></div>
 			<div class="mask">
 				<div class="left" @click="prevPage"></div>
-				<div class="center"></div>
+				<div class="center" @click="toggleTitleAndMenu"></div>
 				<div class="right" @click="nextPage"></div>
 			</div>
 		</div>
+	<transition name="slide-up">
+		<div class="menu-wrapper" v-show="ifTitleAndMenuShow">
+			<div class="icon-wrapper">
+				<span class="iconfont icon icon-menu">&#xe655;</span>
+			</div>
+			<div class="icon-wrapper">
+				<span class="iconfont icon icon-progress">&#xe600;</span>
+			</div>
+			<div class="icon-wrapper">
+				<span class="iconfont icon icon-bright">&#xe614;</span>
+			</div>
+			<div class="icon-wrapper">
+				<span class="iconfont icon icon-font">&#xe64c;</span>
+			</div>
+		</div>
+	</transition>
 	</div>
 </template>
 
 <script>
 import Epub from 'epubjs'
-const DOWNLOAD_URL = '/static/2018_Book.epub'	//载入下载电子书
+const DOWNLOAD_URL = '/static/2019_Book.epub'	//载入下载电子书
 global.ePub = Epub   // 设置全局的ePub对象
 
 /*eslint-disable space-before-funtion-paren */
 
 export default {
+	data() {
+		return {
+			ifTitleAndMenuShow: false,
+		}
+	},
 	methods:{
 		//电子书的解析和渲染
 		showEpub () {
@@ -35,6 +74,7 @@ export default {
 			//通过Rendtion.display渲染电子书
 			this.rendition.display()
 		},
+		//翻页功能
 		prevPage () {
 			//rendition.prev方法实现上一页
 			if(this.rendition){
@@ -45,8 +85,14 @@ export default {
 			//rendition.next方法实现下一页
 				this.rendition.next();
 		},
+		//标题栏和菜单栏
+		toggleTitleAndMenu () {
+			//切换
+			this.ifTitleAndMenuShow=!this.ifTitleAndMenuShow;
+		},
 	},
 	mounted() {
+		//页面加载后
 		this.showEpub()
 	}
 }
@@ -64,9 +110,36 @@ $fontSize: 37.5;
 	justify-content: center;
 	align-items: center;
 }
-.ebook {
+
+.ebook {	
 	position: relative;
-	
+	.title-wrapper {
+			position:absolute;
+			top:0;
+			left:0;
+			z-index:101;
+			width:100%;
+			display:flex;
+			height:px2rem(48);
+			background:white;
+			box-shadow: 0 px2rem(8) px2rem(8) rgba(0,0,0,.15);
+		.left {
+			flex: 0 0 px2rem(60);
+			@include center;
+		}	
+		.right {
+			flex: 1;
+			display:flex;
+			justify-content:flex-end;
+			.icon-wrapper {
+				flex: 0 0 px2rem(40);
+				@include center;
+				.icon-cart {
+					font-size:px2rem(22);
+				}
+			}
+		}
+	}
 	.read-wrapper {
 		.mask {
 			position: absolute; 
@@ -86,7 +159,27 @@ $fontSize: 37.5;
 			}
 		}
 	}
-	
+	.menu-wrapper {
+			position:absolute;
+			bottom:0;
+			left:0;
+			z-index:101;
+			display:flex;
+			width:100%;
+			height:px2rem(48);
+			background:white;
+			box-shadow: 0 px2rem(-8) px2rem(8) rgba(0,0,0,.15);
+			.icon-wrapper {
+				flex:1;
+				@include center;
+				.icon-progress {
+					font-size:px2rem(28)
+				}
+				.icon-bright {
+					font-size:px2rem(24)
+				}
+			}	
+	}
 }
 
 </style>
